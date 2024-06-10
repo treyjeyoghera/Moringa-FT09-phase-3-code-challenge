d767c34f89d7fc3f383e1b3141900811b105f5f0
treyjeyoghera <162321133+treyjeyoghera@users.noreply.github.com>
2024-06-10 14:37:27 +0300

    Update article.py

--- models/article.py
+++ models/article.py
@@ -1,3 +1,10 @@
+# models/Article.py
+
+import sqlite3
+from database.connection import get_db_connection
+from models.author import Author
+from models.magazine import Magazine
+
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
         self.id = id
@@ -6,5 +13,36 @@ class Article:
         self.author_id = author_id
         self.magazine_id = magazine_id
 
-    def __repr__(self):
-        return f'<Article {self.title}>'
+    def __str__(self):
+        magazine = self.get_magazine()
+        magazine_name = magazine.name if magazine else "Unknown Magazine"
+        return f'< {magazine_name} {self.title} {self.content[:30]}...>'
+
+    def save(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO articles (author_id, magazine_id, title, content) VALUES (?, ?, ?, ?)",
+                       (self.author_id, self.magazine_id, self.title, self.content))
+        self.id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+
+    def get_author(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM authors WHERE id = ?", (self.author_id,))
+        author_data = cursor.fetchone()
+        conn.close()
+        if author_data:
+            return Author(author_data["id"], author_data["name"])
+        return None
+
+    def get_magazine(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM magazines WHERE id = ?", (self.magazine_id,))
+        magazine_data = cursor.fetchone()
+        conn.close()
+        if magazine_data:
+            return Magazine(magazine_data["id"], magazine_data["name"], magazine_data["category"])
+        return None
