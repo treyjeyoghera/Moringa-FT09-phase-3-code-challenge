d3b354bb38ff24ec3888a6debc2c460a85aad3ab
treyjeyoghera <162321133+treyjeyoghera@users.noreply.github.com>
2024-06-10 14:38:41 +0300

    Update author.py

--- models/author.py
+++ models/author.py
@@ -1,3 +1,8 @@
+# models/author.py
+
+import sqlite3
+from database.connection import get_db_connection
+
 class Author:
     def __init__(self, id, name):
         self.id = id
@@ -5,3 +10,52 @@ class Author:
 
     def __repr__(self):
         return f'<Author {self.name}>'
+
+    def save(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO authors (name) VALUES (?)", (self.name,))
+        self.id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        if value is not None and not isinstance(value, int):
+            raise ValueError("ID must be an integer")
+        self._id = value
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError("Name must be a non-empty string")
+        self._name = value
+
+    def get_articles(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE author_id = ?", (self.id,))
+        articles = cursor.fetchall()
+        conn.close()
+        return articles
+
+    def get_magazines(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT DISTINCT magazines.*
+            FROM magazines
+            JOIN articles ON magazines.id = articles.magazine_id
+            WHERE articles.author_id = ?
+        """, (self.id,))
+        magazines = cursor.fetchall()
+        conn.close()
+        return magazines
